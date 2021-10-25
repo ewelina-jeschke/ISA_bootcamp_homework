d53f43d027eab627452f48b1b631701cf426bbe3
KMdev <karolmularski94@gmail.com>
2021-10-25 11:28:43 +0200

    Update class_user.py

--- Python homework_09.10-10.10.:homework4/class_user.py
+++ Python homework_09.10-10.10.:homework4/class_user.py
@@ -6,19 +6,23 @@ from class_human import Human
 class User(Human):
     def __init__(self, first_name, last_name, date_of_birth, age=0):
         super().__init__(self, first_name, last_name, date_of_birth)
-        self.age = age
-
+#         self.age = age
+### Nie ma sensu dawać możliwości ustawiania tego ręcznie
 
     @property
     def age(self):
         today = date.today()
-        date_of_birth = datetime.date(2015, 3, 12)
-        self.age = today.year - date_of_birth.year
-        return self.age
+#         date_of_birth = datetime.date(2015, 3, 12)
+#         self.age = today.year - date_of_birth.year
+
+        ### załóżmy że self.datetime == '11-05-1994'
+        birth = datetime.strptime(self.date_of_birth, '%d-%m-%Y')
+# Ta odpowiedź jest bardzo mało dokładna bo nie uwzględnia lat przestępnych ani miesięcy ale do nauki wystarczy
+        return today.year - birth.year
 
 
-
-    @age.setter
-    def age(self, value):
-        self.age = value
+### setter nie jest potrzebny jeśli ma być to tylko wartość obliczana na bieżąco
+#     @age.setter
+#     def age(self, value):
+#         self.age = value
 
